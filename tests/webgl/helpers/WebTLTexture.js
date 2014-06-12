0f428b2f8bb74976dc8bcbceefbe756d10577543
Glenn Adams <glenn@skynav.com>
2014-06-12 13:34:08 -0600

    Clean build.

--- tests/webgl/helpers/WebTLTexture.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-function getWebGLTexture() {
-    var context = document.createElement('canvas').getContext('webgl');
-    if (!context['createTexture'])
-        throw InstantiationError('WebGLContext.createTexture is not defined');
-    return context.createTexture();
-}
