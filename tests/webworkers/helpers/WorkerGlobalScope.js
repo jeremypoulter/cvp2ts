afe511987fe5015c7fdee4ff28bfa985279b87c1
Glenn Adams <glenn@skynav.com>
2014-06-28 19:05:24 -0600

    Update fetches.

--- tests/webworkers/helpers/WorkerGlobalScope.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-function getWorkerGlobalScopeAsync(test) {
-    var worker = new Worker('./resources/workerglobalscope.js');
-    worker.onmessage = test.step_func_done(function (event) {alert(event.data);});
-    worker.postMessage('doit');
-}
