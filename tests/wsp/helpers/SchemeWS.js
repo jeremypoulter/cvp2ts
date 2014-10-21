778e47ed916a4e0d6ae5d2ea13c3866f12854dc7
Glenn Adams <glenn@skynav.com>
2014-10-21 14:38:09 -0600

    Updatte wsp helper.

--- tests/wsp/helpers/SchemeWS.js
+++ tests/wsp/helpers/SchemeWS.js
@@ -14,16 +14,19 @@ function run(tOuter) {
         ws.send(data);
         assert_equals(ws.bufferedAmount, data.length);
         tData = async_test(defProperties.expandedName + '-check-data');
+        ws.onmessage = tData.step_func_done(function(evt) {
+            assert_equals(evt.data, data);
+            ws.close();
+            tDone = async_test(defProperties.expandedName + '-check-done');
+            ws.onclose = tDone.step_func_done(function(evt) {
+                assert_true(open);
+                open = false;
+                assert_true(evt.wasClean);
+            });
+        });
     });
-    ws.onmessage = tData.step_func_done(function(evt) {
-        assert_equals(evt.data, data);
-        ws.close();
-        tDone = async_test(defProperties.expandedName + '-check-done');
+    ws.onerror = tOpen.step_func_done(function() {
+        assert_unreached('web socket error');
     });
-    ws.onclose = tDone.step_func_done(function(evt) {
-        assert_true(open);
-        open = false;
-        assert_true(evt.wasClean);
-        tOuter.done();
-    });
+    tOuter.done();
 }
